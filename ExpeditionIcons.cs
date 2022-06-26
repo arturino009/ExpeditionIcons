b370011784cf00b605573f53749fac31efceb19f
TXKO <82598683+TXKO@users.noreply.github.com>
2022-06-26 10:44:31 -0500

    Update ExpeditionIcons.cs

--- ExpeditionIcons.cs
+++ ExpeditionIcons.cs
@@ -765,7 +765,8 @@ namespace ExpeditionIcons
                         (mods.Any(x => x.Contains("ExpeditionRelicModifierCullingStrikeTwentyPercent")) && Settings.Culling.Value) ||
                         (mods.Any(x => x.Contains("ExpeditionRelicModifierElitesRegenerateLifeEveryFourSeconds")) && Settings.Regen.Value) ||
                         (mods.Any(x => x.Contains("ExpeditionRelicModifierExpeditionCorruptedItemsElite")) && Settings.CorruptedItems.Value) ||
-                        (mods.Any(x => x.Contains("ExpeditionRelicModifierAttackBlockSpellBlockMaxBlockChance")) && Settings.BlockChance.Value))
+                        (mods.Any(x => x.Contains("ExpeditionRelicModifierAttackBlockSpellBlockMaxBlockChance")) && Settings.BlockChance.Value) ||
+                        (mods.Any(x => x.Contains("ExpeditionRelicModifierMaxResistancesAllResistances")) && Settings.MaxResistances.Value))
                         
                      
                        
