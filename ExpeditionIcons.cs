ad12f544e25c7ab30f336ff59c3e2a2656d47a94
arturino009 <arturino009@gmail.com>
2022-09-23 19:02:38 +0300

    Archnem highlighting

--- ExpeditionIcons.cs
+++ ExpeditionIcons.cs
@@ -421,7 +421,44 @@ namespace ExpeditionIcons
                         }
                     }
                 }
+                foreach (var e in GameController.EntityListWrapper.OnlyValidEntities)
+                {
+                    var mobComponent = e?.GetComponent<ObjectMagicProperties>();
+                    if (mobComponent == null) continue;
+                    var modsMonster = mobComponent.Mods;
+                    if (modsMonster.Any(x => x.Contains("Kitava")) ||
+                        modsMonster.Any(x => x.Contains("Lunaris")) ||
+                        modsMonster.Any(x => x.Contains("Shakari")) ||
+                        modsMonster.Any(x => x.Contains("Solaris")) ||
+                        modsMonster.Any(x => x.Contains("Innocence")) ||
+                        modsMonster.Any(x => x.Contains("Arakaali")) ||
+                        modsMonster.Any(x => x.Contains("BrineKing")) ||
+                        modsMonster.Any(x => x.Contains("Wealthy"))
+                        )
+                    {
+                        var positionedComp = e.GetComponent<Positioned>();
 
+                        var text = "RARE";
+
+                        artifacts.Add(e);
+
+                        //if (modelPath == null) continue;
+                        //text = text && modelPath.Substring(0, modelPath.IndexOf("."));
+
+                        var TextInfo = new MinimapTextInfo
+                        {
+                            Text = text,
+                            FontSize = 16,
+                            FontColor = Color.Black,
+                            FontBackgroundColor = Color.White,
+                            TextWrapLength = 35
+                        };
+                        var ent = new StoredEntity(e.GetComponent<Render>().Z, positionedComp.GridPos, e.Id, TextInfo);
+                        if (GameController.Game.IngameState.IngameUi.Map.LargeMap.IsVisible)
+                            DrawToLargeMiniMapText(ent, ent.TextureInfo);
+                        continue;
+                    }
+                }
                 foreach (var e in GameController.EntityListWrapper.ValidEntitiesByType[EntityType.IngameIcon])
                 //foreach (var e in GameController.EntityListWrapper.NotOnlyValidEntities)
                 {
